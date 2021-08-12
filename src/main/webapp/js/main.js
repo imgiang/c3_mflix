9e07d5d12685be86f19155d3e0eca70664b4e9e6
imgiang <imgiang>
2021-08-12 12:40:08 +0700

    web 5

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,68 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA-nvDEsOdu3ypY5-hQZaO68DdEFs3_fHA",
+    authDomain: "c3mflix.firebaseapp.com",
+    projectId: "c3mflix",
+    storageBucket: "c3mflix.appspot.com",
+    messagingSenderId: "864624225868",
+    appId: "1:864624225868:web:d43d5cb6e62fe8c2e34ee9",
+    measurementId: "G-N0P25FDNPW"
+};
+
+var uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        "microsoft.com",
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult) {
+            if (authResult.user) {
+                handleSignedInUser(authResult.user);
+            }
+            return false;
+        },
+        signInFailure: function (error) {
+
+        }
+    },
+
+    autoUpgradeAnonymousUsers: true
+};
+
+var ui
+$(function () {
+    firebase.initializeApp(firebaseConfig);
+    ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+    firebase.auth().onAuthStateChanged(function (user) {
+        user ? handleSignedInUser(user) : handleSignedOutUser();
+        $("#login-spinner").addClass("d-none")
+    });
+});
+
+function handleSignedInUser(user) {
+    $(".user").removeClass("d-none")
+    $(".guest").addClass("d-none")
+
+    $("#name").text(user.displayName);
+    $("#email").text(user.email);
+    $("#phone").text(user.phoneNumber);
+    if (user.photoURL) {
+        $(".avatar").attr("src",user.photoURL);
+    } else {
+        $(".avatar").attr("src","/images/user.svg");
+    }
+    $('#modal-login').modal('hide');
+}
+
+function handleSignedOutUser() {
+    ui.start("#firebaseui-auth-container", uiConfig);
+    $(".user").addClass("d-none")
+    $(".guest").removeClass("d-none")
+}
